fcaa9892ea89f793e4751ba114b76951a03b9b6c
Beondel <bengmacmillan@gmail.com>
2018-02-04 21:59:02 -0800

    started entropy website

---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def entropy(text):
+    charArray = list(text.replace(" ", ""))
+    data = [ord(i) for i in charArray]
+
+    prob_dict = {x:data.count(x)/len(data) for x in data}
+    probs = np.array(list(prob_dict.values()))
+
+    return -probs.dot(np.log2(probs))
+
+print(entropy(text))
